5739c85a3180d9d5f82fb61db0dd8949d19e0ca7
cutiekate <cutiekate@outlook.com>
2020-11-02 16:14:42 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 new WOW().init();
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        autoplay: true,
+        nav: true,
+        dots: true,
+        responsiveClass:true,
+        responsive:{
+        414:{
+            items:1,
+            nav:true
+        },
+        375:{
+            items:1,
+            nav:true
+        },
+        320:{
+            items:1,
+            nav:true
+        },
+        425:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2,
+            nav:false
+        },
+        1600:{
+            items:3,
+            nav:true,
+        }
+    }
+    });
+  });
+
+function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("content");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(tabName).style.display = "flex";  
+}
