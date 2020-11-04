32874ec4dd43af0d66567fe68aa3a6f5f21d09e3
cutiekate <cutiekate@outlook.com>
2020-11-04 12:31:29 +0200

    buttons effect added

--- js/main.js
+++ js/main.js
@@ -44,3 +44,35 @@ function openTab(tabName) {
     }
     document.getElementById(tabName).style.display = "flex";  
 }
+
+var sections = $('section')
+  , nav = $('.scroll-panel')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
