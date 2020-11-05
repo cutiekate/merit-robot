eec7fe5cc0f6cf96f4414f280844d722dd98d7de
cutiekate <cutiekate@outlook.com>
2020-11-05 11:44:53 +0200

    done

--- js/main.js
+++ js/main.js
@@ -76,3 +76,21 @@ nav.find('a').on('click', function () {
   
   return false;
 });
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+$(document).ready(function(){
+  $("#menu-2").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
